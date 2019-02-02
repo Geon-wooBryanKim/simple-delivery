08c302c05b0998d3ca2317f45ec7424b39de8b7a
Jaeyoung Shim <shim@Jaeyoungui-MacBook-Pro.local>
2019-02-03 01:29:12 +0900

    connect node with mysql

--- server/index.js
+++ server/index.js
@@ -1,3 +1,24 @@
-const express = require('express');
-
-console.log('hello world!');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'wodud1078!',
+  database : 'testdb'
+});
+ 
+connection.connect(function(err){
+    if(err){
+        console.error('error connectiong' + err.stack);
+        return ;
+    }
+    console.log('connected as id : ' + connection.threadId);
+});
+ 
+connection.query('SELECT * FROM user', function (error, results, fields) {
+  if (error) {
+      throw error;
+  }
+  console.log('The solution is: ', results);
+});
+ 
+connection.end();
