142eb27ae4fdd84398bcec114121bea9db43d2a1
Jaeyoung Shim <shim@Jaeyoungui-MacBook-Pro.local>
2019-02-11 02:02:27 +0900

    Apply .gitignore

--- server/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    secret: 'worldisfullofdevelopers',
-
-    //node.js 와 mysql 연동을 위한 config
-    host     : 'localhost',
-    user     : 'root',
-    password : 'wodud1078!',
-    database : 'testdb'
-}
